0c5fb454ccca52a4220c89daace0052a9ae66f97
kipkoechemmanuelgit-cmyk <kipkoechemmanuelgit@gmail.com>
2025-10-14 10:42:22 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'securitychat-v2';
+const CACHE_NAME = 'securechat-v1';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -18,19 +18,5 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });
